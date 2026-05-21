98535158916d2e0c3b5f390c3d50075f556a370a
hoanganvn56 <phamhoangan56@gmail.com>
2026-05-21 09:22:51 +0700

    Add algorithm description for temperature conversion
    Add algorithm description for temperature conversion from Fahrenheit to Celsius.

--- cau2.py
+++ cau2.py
@@ -1,1 +1,14 @@
+/*
+Câu 2: Đổi nhiệt độ từ độ F  sang độ C
+
+Mô tả thuật toán bằng ngôn ngữ tự nhiên:
+
+Bước 1 (Nhập): Nhập giá trị nhiệt độ tính theo độ F (số thực) từ bàn phím.
+
+Bước 2 (Xử lý): Áp dụng công thức chuyển đổi từ độ F sang độ C:
+
+C = (F-32)x5/9
+
+Bước 3 (Xuất): In giá trị nhiệt độ đã đổi sang độ C ra màn hình (có thể làm tròn đến 2 chữ số thập phân cho đẹp).
+*/
 
